a8cb98b6e2c7d1bd77fbf4e384810b85cad58f18
Sergey Berezovskiy <sergey.berezovskiy@gmail.com>
2016-06-05 12:43:21 +0300

    Tests for missing and done

--- SimpleDotComGame/src/test/java/com/headfirst/SimpleDotComTest.java
+++ SimpleDotComGame/src/test/java/com/headfirst/SimpleDotComTest.java
@@ -11,15 +11,30 @@ public class SimpleDotComTest {
     @Before
     public void setUp() throws Exception {
         dotCom = new SimpleDotCom();
+        int[] locations = {2,3,4};
+        dotCom.setLocationCells(locations);
     }
 
     @Test
     public void shouldHitWhenGuessIsCorrect() throws Exception {
-        int[] locations = {2,3,4};
-        dotCom.setLocationCells(locations);
-
         String result = dotCom.checkYourself("2");
 
         assertEquals("Hit", result);
+    }
+
+    @Test
+    public void shouldMissWhenGuessIsNotCorrect() throws Exception {
+        String result = dotCom.checkYourself("1");
+
+        assertEquals("Miss", result);
+    }
+
+    @Test
+    public void shouldBeoneAfterThreeCorrectGuesses() throws Exception {
+        dotCom.checkYourself("2");
+        dotCom.checkYourself("3");
+        String result = dotCom.checkYourself("4");
+
+        assertEquals("Done", result);
     }
 }
