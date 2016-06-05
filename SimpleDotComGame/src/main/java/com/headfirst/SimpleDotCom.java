74636d6884a0c5061eb6b4ab3f139a239b6e4fe2
Sergey Berezovskiy <sergey.berezovskiy@gmail.com>
2016-06-05 12:34:05 +0300

    Making first test run

--- SimpleDotComGame/src/main/java/com/headfirst/SimpleDotCom.java
+++ SimpleDotComGame/src/main/java/com/headfirst/SimpleDotCom.java
@@ -3,12 +3,22 @@ package com.headfirst;
 public class SimpleDotCom {
 
     private int[] locationCells;
+    private int hitCount;
 
     public void setLocationCells(int[] locationCells) {
         this.locationCells = locationCells;
     }
 
     public String checkYourself(String userGuess) {
-        return null;
+        int target = Integer.parseInt(userGuess);
+
+        for(int cell : locationCells) {
+            if (cell == target) {
+                hitCount++;
+                return hitCount == locationCells.length ? "Done" : "Hit";
+            }
+        }
+
+        return "Miss";
     }
 }
